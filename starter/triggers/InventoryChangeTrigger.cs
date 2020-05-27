b2f10c5b1d5414931b9eb25d66bceb6ab98e5e58
Eric <rothes.work@gmail.com>
2020-05-27 09:41:42 -0400

    Add triggers.

---
+++ starter/triggers/InventoryChangeTrigger.cs
@@ -0,0 +1,44 @@
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryManagement
+{
+    public static class OnHandChangeTrigger
+    {
+        [FunctionName("OnHandChangeTrigger")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "CosmosDatabaseName",
+            collectionName: "CosmosOnHandContainerName",
+            ConnectionStringSetting = "CosmosConnectionString",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> documents,
+            ILogger log)
+        {
+            if (documents != null && documents.Count > 0)
+            {
+                log.LogInformation("Documents modified: {count}", documents.Count);
+                log.LogInformation("First document Id: {id}", documents[0].Id);
+            }
+        }
+    }
+    public static class ShipmentChangeTrigger
+    {
+        [FunctionName("ShipmentChangeTrigger")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "CosmosDatabaseName",
+            collectionName: "CosmosShipmentContainerName",
+            ConnectionStringSetting = "CosmosConnectionString",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> documents,
+            ILogger log)
+        {
+            if (documents != null && documents.Count > 0)
+            {
+                log.LogInformation("Documents modified: {count}", documents.Count);
+                log.LogInformation("First document Id: {id}", documents[0].Id);
+            }
+        }
+    }
+}
